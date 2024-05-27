81bdeaed8c74b0202795f7a36827b30fa48874b4
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-27 23:01:18 +0200

    end project

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,19 +7,18 @@
             @foreach ($comics as $comic)
                 <div class="col-4">
                     <div class="card">
-                        <img src="{{-- $comic->thumb --}} https://upload.wikimedia.org/wikipedia/en/thumb/5/5a/Action_Comics_1.jpg/200px-Action_Comics_1.jpg" class="card-img-top" alt="{{ $comic->title }}">
+                        <img src="{{-- $comic->thumb --}} https://upload.wikimedia.org/wikipedia/en/thumb/5/5a/Action_Comics_1.jpg/200px-Action_Comics_1.jpg"
+                            class="card-img-top" alt="{{ $comic->title }}">
                         <div class="card-body">
                             <h5>{{ $comic->title }}</h5>
                             <div><strong>Series:</strong> {{ $comic->series }}</div>
                             <div><strong>Type:</strong> {{ $comic->type }}</div>
                             <div><strong>Price:</strong> {{ $comic->price }}</div>
-                            <div>
-                                <a rel="stylesheet" href="{{route('comics.destroy', $comic->id)}}">Info</a>
-                            </div>
-                            <div>
-                                <a rel="stylesheet" href="{{route('comics.edit', $comic->id)}}">Edit</a>
-                            </div>
-                            <form action="{{ route('comics.destroy', ['comic' => $comic->id])}}" method="POST">
+                            <a rel="stylesheet" href="{{ route('comics.destroy', $comic->id) }}"
+                                class="btn btn-primary">Info</a>
+                            <a rel="stylesheet" href="{{ route('comics.edit', $comic->id) }}"
+                                class="btn btn-secondary my-2">Edit</a>
+                            <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST" class="d-inline">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger">Delete</button>
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -13,9 +13,13 @@
             <div><strong>Series:</strong> {{ $comic->series }}</div>
             <div><strong>Type:</strong> {{ $comic->type }}</div>
             <div><strong>Price:</strong> {{ $comic->price }}</div>
-            <div>
-                <a rel="stylesheet" href="{{route('comics.edit', $comic->id)}}">Edit</a>
-            </div>
+            <a rel="stylesheet" href="{{ route('comics.edit', $comic->id) }}"
+                class="btn btn-secondary my-2">Edit</a>
+            <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST" class="d-inline">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
         </div>
     </div>
 </div>
