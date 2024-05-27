6a62313ad7addeae96e783f1d8b91de0aee0b474
WilliamHaxhija <williamhaxhija@gmail.com>
2024-05-27 22:44:54 +0200

    feat: add delete

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -10,16 +10,20 @@
                         <img src="{{-- $comic->thumb --}} https://upload.wikimedia.org/wikipedia/en/thumb/5/5a/Action_Comics_1.jpg/200px-Action_Comics_1.jpg" class="card-img-top" alt="{{ $comic->title }}">
                         <div class="card-body">
                             <h5>{{ $comic->title }}</h5>
-                            <div class="overflow-y-auto" style="height: 8rem">
-                                <p>{{ $comic->description }}</p>
-                            </div>
                             <div><strong>Series:</strong> {{ $comic->series }}</div>
                             <div><strong>Type:</strong> {{ $comic->type }}</div>
                             <div><strong>Price:</strong> {{ $comic->price }}</div>
                             <div>
-                                <a rel="stylesheet" href="{{route('comics.show', $comic->id)}}">Info</a>
+                                <a rel="stylesheet" href="{{route('comics.destroy', $comic->id)}}">Info</a>
                             </div>
-                            <a rel="stylesheet" href="{{route('comics.edit', $comic->id)}}">Edit</a>
+                            <div>
+                                <a rel="stylesheet" href="{{route('comics.edit', $comic->id)}}">Edit</a>
+                            </div>
+                            <form action="{{ route('comics.destroy', ['comic' => $comic->id])}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
                         </div>
                     </div>
                 </div>
